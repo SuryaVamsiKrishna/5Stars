ea7fd2a91fe4a94e21d67ef6fc2ae7f902d089c1
saisanthosh111 <44166890+saisanthosh111@users.noreply.github.com>
2022-06-09 17:41:06 +0530

    Updating the code

--- 5Stars/src/com/petclinic/models/Pet.java
+++ 5Stars/src/com/petclinic/models/Pet.java
@@ -1,5 +1,34 @@
 package com.petclinic.models;
 
 public class Pet {
+private String name;
+private String type;
+private static long id = 100;
+public Pet(String name, String type) {
+this.name = name;
+this.type = type;
+this.id = id++;
+}
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public String getType() {
+return type;
+}
+public void setType(String type) {
+this.type = type;
+}
+public static long getId() {
+return id;
+}
 
+
+
+@Override
+public String toString() {
+return "Pet [name=" + name + ", type=" + type + "]";
+}
 }
